3219b92857bf612240893e27cf60c1d95569c25c
Sarfaraz <m.s.hussain@outlook.com>
2021-06-22 21:22:03 -0500

    redone the assingment and added js functionality

--- script.js
+++ script.js
@@ -1,25 +1,89 @@
-// p;ull in page objects
-var highscoreDiv = document.querySelector("#highscore");
-var gameTimerEl = document.querySelector("#gameTimer");
-var quesTimerEl = document.querySelector("#quesTimer");
-var mainEl = document.querySelector("#details");
-var timerTab = document.querySelector("#timers");
 
 
-var questionEl = document.querySelector("#question")
-var answersListEl = document.querySelector("#answer-list")
 
+var questions = [
+    {
+        title: "Hyper Text Markup Language Stand For?",
+        choices: ["JavaScript", "XHTML","CSS", "HTML"],
+        answer: "HTML"
+    },
 
-var test = false;
-var score = 0;
-var quiz = {};
-var quizType = "";
+    {
+        title: "Which language is used for styling web pages?",
+        choices: ["HTML", "JQuery", "CSS", "XML"], 
+        answer: "CSS"
+    },
 
-var gameDuration = 0;
-var gameSecElapsed = 0;
-var gameInterval;
+    {
+        title: "Which is not a JScript Framework?",
+        choices: ["Python Script", "JQuery","Django", "Node"],
+        answer: "Django"
+    },
 
-var questionDuration = 15;
-var questionSecElapsed = 0;
-var questionInterval;
+    {
+        title: "Which is used for Connect To Database?",
+        choices: ["PHP", "HTML", "JS", "All"],
+        answer: "PHP"
+    }
+]
 
+// function Quiz(questions) {
+//     this.score = 0;
+//     this.questions = questions;
+//     this.questionIndex = 0;
+// } 
+.
+
+$("#initBtn").on("click", function firstQ() { 
+    $("#question").text(questions[0].title)
+    $("#btn0").text(questions[0].choices[0])
+    $("#btn1").text(questions[0].choices[1])
+    $("#btn2").text(questions[0].choices[2])
+    $("#btn3").text(questions[0].choices[3])   
+
+} )
+
+var i = 0
+
+function cycle() { 
+    i++; 
+    $("#question").text(questions[i].title)
+    $("#btn0").text(questions[i].choices[0])
+    $("#btn1").text(questions[i].choices[1])
+    $("#btn2").text(questions[i].choices[2])
+    $("#btn3").text(questions[i].choices[3]) 
+    
+}
+var score = 0
+$("#btn0").on("click", function(){
+    if($("#btn0").text() === questions[i].answer){
+       score++;
+       console.log(score)
+      
+
+}
+cycle()
+
+$("#score").text("Score: " + score)
+})
+$("#btn1").on("click", function(){
+    if($("#btn1").text() === questions[i].answer){
+       score++;
+    }
+    cycle()
+    $("#score").text("Score: " + score)
+})
+$("#btn2").on("click", function(){
+    if($("#btn2").text() === questions[i].answer){
+       score++;
+    }
+    cycle()
+    $("#score").text("Score: " + score)
+})
+$("#btn3").on("click", function(){
+    if($("#btn3").text() === questions[i].answer){
+       score++;
+    } 
+    cycle()
+    $("#score").text("Score: " + score)
+})
